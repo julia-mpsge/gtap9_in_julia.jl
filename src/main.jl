650a2a338c6baa45be79642950d01c63815947f4
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-05-08 13:01:19 -0500

    Add path license string

--- src/main.jl
+++ src/main.jl
@@ -3,6 +3,10 @@ using gtap9_in_julia
 using gtap9_in_julia.DataFrames
 using gtap9_in_julia.MPSGE
 
+import PATHSolver
+PATHSolver.c_api_License_SetString("2830898829&Courtesy&&&USR&45321&5_1_2021&1000&PATH&GEN&31_12_2025&0_0_0&6000&0_0")
+
+
 data = load_data("g20_10")
 
 gtap  = gtap9(data);
